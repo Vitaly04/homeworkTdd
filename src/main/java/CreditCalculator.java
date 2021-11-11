f470252e6c0c1a01cfc9eda5d9255a430b989540
Vitaly <supercyfqgth2010@yandex.ru>
2021-11-11 19:15:47 +0300

    added realization

--- src/main/java/CreditCalculator.java
+++ src/main/java/CreditCalculator.java
@@ -1,14 +1,20 @@
 public class CreditCalculator {
 
     public double calculationOfMonthlyPayment(double creditAmount, double interestRate, double numberOfPayments) {
-
+        final double TOTALPERCENT = 100;
+        final double MONTHSOFJEAR = 12;
+        double monthlyInterestRate = interestRate / MONTHSOFJEAR / TOTALPERCENT;
+        return (creditAmount * (monthlyInterestRate * (Math.pow((1 + monthlyInterestRate), numberOfPayments))) /
+                ((Math.pow((1 + monthlyInterestRate), numberOfPayments)) - 1));
     }
 
     public double calculationTotalRefundAmount(double creditAmount, double interestRate, double numberOfPayments) {
-
+        double monthlyPayment = calculationOfMonthlyPayment(creditAmount, interestRate, numberOfPayments);
+        return monthlyPayment * numberOfPayments;
     }
 
     public  double calculationOverpayment(double creditAmount, double interestRate, double numberOfPayments) {
-
+        double totalRefundAmount = calculationTotalRefundAmount(creditAmount, interestRate, numberOfPayments);
+        return totalRefundAmount - creditAmount;
     }
 }
